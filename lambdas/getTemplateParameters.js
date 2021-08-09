346f68aa688bcc279a8dd2022bfdb8c229434f14
a96lex <a96lex@gmail.com>
2021-08-09 12:50:15 +0200

    templates handlers

---
+++ lambdas/getTemplateParameters.js
@@ -0,0 +1,50 @@
+import { returnError } from "../helpers/returnError";
+
+const apiaudio = require("apiaudio").default;
+
+const { Sound } = apiaudio;
+const apiKey = process.env.APIKEY;
+
+export const handler = async (event) => {
+  const queryStringParameters = event?.queryStringParameters;
+
+  const debug = queryStringParameters?.debug || false;
+
+  //configure aflr package
+  if (apiaudio.isInitialized) {
+    apiaudio.reset();
+  }
+  try {
+    apiaudio.configure({
+      apiKey,
+      debug,
+    });
+  } catch (e) {
+    console.log(e);
+    return returnError({
+      statusCode: 500,
+      message: "Problem configuring the aflr package.",
+    });
+  }
+
+  let parameters;
+  try {
+    parameters = await Sound.parameters();
+  } catch (e) {
+    console.log(e);
+    return returnError({
+      statusCode: 500,
+      message: "Problem retrieving the parameters.",
+    });
+  }
+
+  // return TTS URL
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify(parameters),
+  };
+};
